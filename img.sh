603238c0df2234b21e63d46694ca68667355015c
bigzhu <vermiliondun@gmail.com>
2019-08-19 00:06:04 +0800

    修复 img.sh 的 bug

--- img.sh
+++ img.sh
@@ -1,13 +1,13 @@
-blog_img_path="~/Project/sites/blog/cheese/assets/img"
+blog_img_path="$HOME/Project/sites/blog/cheese/assets/img"
 # 剔除输入的文件的路径
-file_name=$(basename $1)
+file_name=$(basename "$1")
+name=$(echo "$file_name" | sed -e 's/\.[^.]*$//')
+echo "compress $file_name"
 # 删除第一个.后的内容(后缀)
-name=${file_name%%.*}
 new_file_name="$name.jpg"
-echo $file_name, $name, $new_file_name
+#echo $file_name, $name, $new_file_name
 # 压缩图片到对应路径, 依赖 brew install mozjpeg
-command="cjpeg -quality 44 $1 > $blog_img_path/$new_file_name"
-eval $command
+cjpeg -quality 44 "$1" > "$blog_img_path/$new_file_name"
 
 # markdow 格式
 img="![$new_file_name](/assets/img/$new_file_name)"
