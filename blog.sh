02c2aa93b78794ce131e1d72f2419b99f8fd2dfc
bigzhu <vermiliondun@gmail.com>
2020-01-17 14:03:20 +0800

    get real blog path

--- blog.sh
+++ blog.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 rm -rf ./public/*
-cd /Users/bigzhu/Project/sites/blog
+DIR="$(dirname "$(readlink -f "$0")")"
+echo $DIR
+cd $DIR
+#cd /Users/bigzhu/Project/sites/blog
 hugo;
 rsync --delete -rvzt  -e "ssh" ./public/* bigzhu@my.vps:/home/bigzhu/hugo_blog/public --delete-after
