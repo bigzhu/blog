ed74468483637bed7d2fe7249d6347f3edfdec52
bigzhu <bigzhu@192.168.1.4>
2021-07-22 18:15:55 +0800

    new config

--- blog.sh
+++ blog.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-SYNC_TO="bigzhu@bigzhu.net:/home/bigzhu/hugo_blog/public"
-cd /Users/bigzhu/blog
+SYNC_TO="bigzhu@198.44.14.30:/home/bigzhu/hugo_blog/public"
+cd /Users/bigzhu/Projects/blog
 rm -rf ./public/*
 hugo;
 rsync --delete -rvzt  -e "ssh" ./public/* $SYNC_TO --delete-after
